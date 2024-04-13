634aafbcf081e7f4f1ecebb85a7aaf7a9220fc89
zhixian <zhixian@hotmail.com>
2024-04-13 09:21:58 +0800

    add credentials interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule
+    , IonicModule.forRoot()
+    , AppRoutingModule
+    , HttpClientModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    , provideHttpClient(withInterceptors([credentialsInterceptor]))
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
