5df477e3dab12f8e084c1194668ce941a474706c
zhixian <zhixian@hotmail.com>
2024-04-16 10:01:13 +0800

    Add WebApiService

---
+++ src/app/services/web-api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebApiService {
+
+  private baseUrl: string;
+
+  constructor() {
+    this.baseUrl = "http://localhost:7265";
+  }
+
+  UrlFor(key:string) {
+    switch (key.toUpperCase()) 
+    {
+      case "AUTHENTICATEUSER":
+        return `${this.baseUrl}/api/AuthenticateUser`
+      case "HELLOWORLD":
+        return `${this.baseUrl}/api/HelloWorld`
+      default:
+        return "";
+    }
+  }
+}
