0c96929ce9700139c7020bbb47fa2427f89b1987
zhixian <zhixian@hotmail.com>
2024-04-17 14:01:18 +0800

    Add guards

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,43 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+
+export const authGuard: CanActivateFn = async (route, state) => {
+  console.log("in authGuard");
+  
+  let hasAuthenticatedUser = await inject(AuthenticationService).hasAuthenticatedUser();
+
+  console.log(`In authGuard ${hasAuthenticatedUser}`);
+
+  return hasAuthenticatedUser ? true : inject(Router).navigate(['/login']);
+
+  //return true;
+  // return await inject(AuthenticationService).hasAuthenticatedUser() 
+  //   ? true 
+  //   : inject(Router).navigate(['/login']);
+
+  // const userProfile = this.authenticationService.userProfileValue;
+  
+  // // No userProfile => not logged in
+  // // so redirect to login page with the return url
+  // if (!userProfile) {
+      
+  //     this.router.navigate(['/login'], { 
+  //         queryParams: { returnUrl: state.url } 
+  //     });
+
+  //     return false;
+  // }
+  //return inject(Router).createUrlTree(['/login']);
+  
+
+  // Else is logged in so return true
+  //return true;
+
+  // Angular 16 (see: https://medium.com/@ojiofor/how-to-use-angular-canactivate-function-b153e5a79f51)
+  // // Assume we have constructor(private token: TokenService, private router: Router) { }
+  // return inject(TokenService).authenticated()
+  //   ? true
+  //   : inject(Router).createUrlTree(['/auth/login']);
+  
+};
