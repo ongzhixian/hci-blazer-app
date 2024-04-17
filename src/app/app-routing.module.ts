e02831263f311784ed0644a62778531cc1d3674e
zhixian <zhixian@hotmail.com>
2024-04-17 14:58:18 +0800

    add login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: ''
+    , canActivateChild: [authGuard]
+    , loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginFormGroup!: FormGroup;
+
+  constructor(
+    private router: Router
+    , private fb: FormBuilder
+    //, private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit() {
+    this.loginFormGroup = this.fb.group({
+      email: ['zhixian@hotmail.com', [Validators.required, Validators.email]],
+      password: ['password1234', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  signIn() {
+    console.log(`Attempting to sign-in ${this.loginFormGroup.value.email}, ${this.loginFormGroup.value['password']}`);
+
+    // this.authenticationService.authenticateUser(
+    //   this.loginFormGroup.value.email
+    //   , this.loginFormGroup.value.password);
   }
 
 }
