e43f2c76f15f47886cca475b9cf55a3d22c7f7a2
zhixian <zhixian@hotmail.com>
2024-04-18 11:24:36 +0800

    working migrated tab 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,34 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  @ViewChild(IonModal) modal!: IonModal;
 
+  updatePasswordFormGroup: FormGroup = this.fb.group({
+    password: ['password12345', [Validators.required, Validators.minLength(6)]],
+    passwordConfirmation: ['password12345', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(
+    private authenticationService: AuthenticationService
+    , private fb: FormBuilder) { }
+
+  signOut() {
+    this.authenticationService.signOut();
+  }
+    // change-password-modal
+
+  // confirm() {
+  //   this.modal.dismiss(null, 'confirm');
+  // }
+
+  dismiss() {
+    console.log("Dismiss change password dialog");
+    this.modal.dismiss(null, 'dismiss');  
+  }
+
+  updatePassword() {
+    console.log(`In updatePassword() ${this.updatePasswordFormGroup.value.password} ${this.updatePasswordFormGroup.value.passwordConfirmation}`);
+    this.modal.dismiss(null, 'confirm');
+  }
+  
 }
